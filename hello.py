57be5c39c6897782a513ed27d7b0006cc760ed45
hanzheng <1035749458@qq.com>
2024-06-11 02:22:06 +0800

    cat verion

--- hello.py
+++ hello.py
@@ -4,7 +4,10 @@ def default():
     if len(sys.argv) != 2:
         print("wrong usage")
         return
-    print("hello world")
+    if sys.argv[1] == "cat":
+        print("meow")
+    else:
+        print("hello world")
 
 def main():
     default()
